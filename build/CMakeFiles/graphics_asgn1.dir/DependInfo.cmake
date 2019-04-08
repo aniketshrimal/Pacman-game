8abd74dc26d7a41c64821b1afe8ad1bd7dfafdc7
aniketshrimal <aniketshrimal98@gmail.com>
2019-04-09 02:59:47 +0530

    remove errors

---
+++ build/CMakeFiles/graphics_asgn1.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/aniket/Pacman-game/src/ball.cpp" "/home/aniket/Pacman-game/build/CMakeFiles/graphics_asgn1.dir/src/ball.cpp.o"
+  "/home/aniket/Pacman-game/src/color.cpp" "/home/aniket/Pacman-game/build/CMakeFiles/graphics_asgn1.dir/src/color.cpp.o"
+  "/home/aniket/Pacman-game/src/input.cpp" "/home/aniket/Pacman-game/build/CMakeFiles/graphics_asgn1.dir/src/input.cpp.o"
+  "/home/aniket/Pacman-game/src/magnet.cpp" "/home/aniket/Pacman-game/build/CMakeFiles/graphics_asgn1.dir/src/magnet.cpp.o"
+  "/home/aniket/Pacman-game/src/main.cpp" "/home/aniket/Pacman-game/build/CMakeFiles/graphics_asgn1.dir/src/main.cpp.o"
+  "/home/aniket/Pacman-game/src/nonedit.cpp" "/home/aniket/Pacman-game/build/CMakeFiles/graphics_asgn1.dir/src/nonedit.cpp.o"
+  "/home/aniket/Pacman-game/src/other_handlers.cpp" "/home/aniket/Pacman-game/build/CMakeFiles/graphics_asgn1.dir/src/other_handlers.cpp.o"
+  "/home/aniket/Pacman-game/src/plank.cpp" "/home/aniket/Pacman-game/build/CMakeFiles/graphics_asgn1.dir/src/plank.cpp.o"
+  "/home/aniket/Pacman-game/src/platform.cpp" "/home/aniket/Pacman-game/build/CMakeFiles/graphics_asgn1.dir/src/platform.cpp.o"
+  "/home/aniket/Pacman-game/src/player.cpp" "/home/aniket/Pacman-game/build/CMakeFiles/graphics_asgn1.dir/src/player.cpp.o"
+  "/home/aniket/Pacman-game/src/pond.cpp" "/home/aniket/Pacman-game/build/CMakeFiles/graphics_asgn1.dir/src/pond.cpp.o"
+  "/home/aniket/Pacman-game/src/porc.cpp" "/home/aniket/Pacman-game/build/CMakeFiles/graphics_asgn1.dir/src/porc.cpp.o"
+  "/home/aniket/Pacman-game/src/timer.cpp" "/home/aniket/Pacman-game/build/CMakeFiles/graphics_asgn1.dir/src/timer.cpp.o"
+  "/home/aniket/Pacman-game/src/trampoline.cpp" "/home/aniket/Pacman-game/build/CMakeFiles/graphics_asgn1.dir/src/trampoline.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/libdrm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
